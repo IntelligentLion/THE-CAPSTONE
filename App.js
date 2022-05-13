8ce47a7d83ed9a560ab90dbda22cb5afe3a1aa41
TNaton <105522135+TNaton@users.noreply.github.com>
2022-05-13 14:38:11 -0700

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
-        
+      Submit a issue
       </Text>
     </View>
   );
