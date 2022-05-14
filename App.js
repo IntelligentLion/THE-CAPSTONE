9156af932cf6507b6d22db48aea8dc55873d04cf
TNaton <105522135+TNaton@users.noreply.github.com>
2022-05-13 17:29:12 -0700

    Update App.js
    I have a bit of issue with it plz fix it

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+
 export default function App() {
+  const [submit, setSubmit] = React.useState(0);
+  const onSubmit = () => {
+  }
+  const[submitButton, setSubmitButton] = React.useState(0);
+  const[content, setContent] = React.useState(0);
+  const onContent = (newContent) => {
+    setContent(newContent);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.paragraph}>
       Submit a issue
       </Text>
+     <TextInput
+   onChangeText={submitButton}
+   value={content}
+   keyboardType="number-pad"
+/>
+      <Button
+  onPress={onSubmit}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
     </View>
   );
 }
